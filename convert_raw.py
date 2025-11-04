a1dfdaba76b798f40647da066f4541f70d9cbc13
Tom Keffer <tkeffer@gmail.com>
2025-11-04 11:37:55 -0800

    Save results to ./echodata_zarr
    This follows the convention used by echopype-examples

--- convert_raw.py
+++ convert_raw.py
@@ -5,9 +5,13 @@
 #
 """
 A module to convert raw echosounder files into Zarr format using echopype and Dask.
+
+Through experimentation, I have found a configuration with 4 workers, 2 threads per worker,
+works well on my 4-core, 8-thread NUC.
 """
 import argparse
 import sys
+import time
 import warnings
 from pathlib import Path
 from typing import Iterable
@@ -18,7 +22,9 @@ from dask.distributed import Client
 from utilities import find_raw_files
 
 usagestr = """%(prog)s -h|--help
-       %(prog)s [--sonar-model={ek60|ek80}] [--no-swap] --out-dir=OUT_DIR inputs ...
+       %(prog)s [--out-dir=OUT_DIR] [--sonar-model={ek60|ek80}] [--no-swap] \\
+                  [--workers=WORKERS] [--threads=THREADS]\\
+                  inputs ...
 """
 
 warnings.simplefilter("ignore", category=DeprecationWarning)
@@ -32,9 +38,11 @@ except ImportError:
     warnings.filterwarnings("ignore", message=".*UnstableSpecificationWarning.*")
 
 def convert(raw_files: Iterable[Path],
-            out_dir: Path | str,
+            out_dir: Path | str = "./echodata_zarr/",
             sonar_model: str = "ek80",
-            use_swap: bool = True):
+            use_swap: bool = True,
+            workers=1,
+            threads=1):
     """
     Converts a list of `.raw` files into zarr format and saves them in a given or default directory.
 
@@ -45,12 +53,12 @@ def convert(raw_files: Iterable[Path],
     Parameters:
     raw_files: Iterable[str]
         A collection of `.raw` files.
-    out_dir: Path|str
-        The directory where converted files will be saved. Required.
+    out_dir: Path|str, optional
+        The directory where converted files will be saved. Default is './echodata_zarr/'.
     sonar_model: str, optional
-        The sonar model type to use for the conversion, defaulting to "ek80".
+        The sonar model type to use for the conversion. Default is "ek80".
     use_swap: bool, optional
-        Indicates whether swapping is enabled during conversion, defaulting to True.
+        Indicates whether swapping is enabled during conversion. Default is True.
 
     Raises:
         None directly by this function.
@@ -58,8 +66,8 @@ def convert(raw_files: Iterable[Path],
     Returns:
         None
     """
-    # Use maximum number of CPUs for Dask Client (defaults)
-    client = Client()
+
+    client = Client(n_workers=workers, threads_per_worker=threads)
     print("Dask Client Dashboard:", client.dashboard_link)
 
     # The directory where the converted files will be saved
@@ -106,8 +114,8 @@ def parse_args():
     )
     parser.add_argument(
         "--out-dir",
-        required=True,
-        help="Output directory. Required.",
+        default="./echodata_zarr/",
+        help="Output directory. Default is './echodata_zarr/'",
     )
     parser.add_argument(
         "--sonar-model",
@@ -121,6 +129,21 @@ def parse_args():
         action="store_true",
         help="Disable use_swap flag when opening raw files (default: enabled)",
     )
+    parser.add_argument(
+        "--workers",
+        dest="workers",
+        type=int,
+        default=4,
+        help="Number of workers for Dask Client (default: 4)",
+    )
+    parser.add_argument(
+        "--threads",
+        dest="threads",
+        type=int,
+        default=2,
+        help="Number of threads per worker (default: 2)",
+    )
+
     return parser.parse_args()
 
 
@@ -134,10 +157,14 @@ if __name__ == '__main__':
         sys.exit(0)
 
     print(f"Found {len(raw_files)} raw files")
+    print(f"Using {args.workers} workers and {args.threads} threads per worker")
 
+    start = time.time()
     convert(
         raw_files=raw_files,
         out_dir=args.out_dir,
         sonar_model=args.sonar_model,
         use_swap=not args.no_swap,
     )
+    stop = time.time()
+    print(f"Conversion completed in {stop - start:.2f} seconds")
