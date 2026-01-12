0b2e7d55ac18e17423dd088c08738cb828eef751
Tom Keffer <tkeffer@gmail.com>
2026-01-11 16:23:31 -0800

    Suppression of DeprecationWarning no longer needed.

--- convert_raw.py
+++ convert_raw.py
@@ -27,17 +27,6 @@ usagestr = """%(prog)s -h|--help
                   inputs ...
 """
 
-warnings.simplefilter("ignore", category=DeprecationWarning)
-# Ignore large graph dask UserWarnings
-warnings.simplefilter("ignore", category=UserWarning)
-# Ignore UnstableSpecificationWarning. If the class is not directly importable, use a message filter
-try:
-    from zarr.errors import UnstableSpecificationWarning
-
-    warnings.simplefilter("ignore", category=UnstableSpecificationWarning)
-except ImportError:
-    warnings.filterwarnings("ignore", message=".*UnstableSpecificationWarning.*")
-
 
 def convert(raw_files: Iterable[Path],
             out_format: str = "zarr",
